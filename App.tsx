68fa3aecbfb1f35260820c297a8bef5f809f3b02
Hiraku Onuma <hiraku00@gmail.com>
2024-01-20 10:51:33 +0400

    1st commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.box}>
+        <View style={styles.moziBox}>
+          <Text style={styles.Text}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus illo a dolorem provident aliquid optio.
+          </Text>
+          <Text style={styles.subText}>Lorem, ipsum dolor.</Text>
+        </View>
+        <View style={styles.gazoBox}>
+          <Image
+            style={{ width: 100, height: 100 }}
+            source={{
+              uri: "https://picsum.photos/id/237/200/300",
+            }}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +26,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  box: {
+    height: 100,
+    width: "100%",
+    borderColor: "lightblue",
+    borderWidth: 1,
+    flexDirection: "row",
+  },
+  moziBox: {
+    flex: 1,
+    backgroundColor: "steelblue",
+    padding: 16,
+    justifyContent: "space-between",
+  },
+  gazoBox: {
+    width: 100,
+    backgroundColor: "powderblue",
+  },
+  Text: {
+    fontSize: 16,
+  },
+  subText: {
+    fontSize: 12,
+    color: "red",
   },
 });
